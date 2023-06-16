57e926d45f1bda184dfb0a44a1a69ef7c8a1e973
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-06-15 22:11:35 -0500

    still working on Jwt Service

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -1,12 +1,42 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
 
 @Service
 public class JwtService {
 
+    // I'll figure out how to do the encryption after setting up JWT successfully
+    private static final String SECRET_KEY = "as3dfasdf9sadfhkl8hgj4t31ghg4";
 
     public String extractUsername(String token) {
-        return null;
+        // not sure why this is underlined?
+        return extractClaim(token, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
