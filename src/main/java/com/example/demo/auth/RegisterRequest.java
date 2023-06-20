42c1abe7931cfa5586e3866d041500592aad9d2c
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-06-19 21:58:39 -0500

    created new classes when authenticating in the controller

---
+++ src/main/java/com/example/demo/auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    // Do I want all these in my login? (Probably but look over the database and see how to implement them
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
