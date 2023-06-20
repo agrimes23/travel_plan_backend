c44f657452c720fd05152c7e16b8184d8b83d7da
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-06-19 21:39:12 -0500

    created a method in app config that fecthes user details, username password etc. Setting up for encryption with bcrypt

--- src/main/java/com/example/demo/config/ApplicationConfig.java
+++ src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -5,9 +5,13 @@ import com.example.demo.UserPlansRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,6 +24,20 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> repository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        //fetches user details, username password etc.
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
