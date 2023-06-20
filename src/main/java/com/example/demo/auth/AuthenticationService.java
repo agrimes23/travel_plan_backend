c5c300471ce5455621291f1831984a84a1f6cfb0
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-06-20 11:48:45 -0500

    making methods for registering and authenticating

---
+++ src/main/java/com/example/demo/auth/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.example.demo.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+            
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+    }
+}
