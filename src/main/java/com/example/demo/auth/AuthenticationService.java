5228abe008ad680c5dcba567e9382d6d36d6498b
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-06-20 11:56:50 -0500

    created register method, might need to reformat it later to include tripPlans

--- src/main/java/com/example/demo/auth/AuthenticationService.java
+++ src/main/java/com/example/demo/auth/AuthenticationService.java
@@ -1,19 +1,38 @@
 package com.example.demo.auth;
 
+import com.example.demo.Role;
+import com.example.demo.UserPlans;
+import com.example.demo.UserPlansRepository;
+import com.example.demo.config.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserPlansRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-            
+        var user = UserPlans.builder()
+                // TODO: might need to put an empty tripPlans list here too, since it's creating a user in the db
+                .username(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
 
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-
+        return null;
     }
 }
